465e4d29956588497470ac40de4ca848bf4e2007
AngeloSolusys <angelo@solusys.com.br>
2021-07-21 13:54:10 -0300

    21/07/2021

--- AddOns.dpr
+++ AddOns.dpr
@@ -2,23 +2,24 @@ program AddOns;
 
 uses
   Vcl.Forms,
-  UntMain in 'UntMain.pas' {FrmMain},
-  UntBibliotecaFuncoes in 'D:\Biblioteca\UntBibliotecaFuncoes.pas',
-  UntBuscaComponentes in 'D:\Biblioteca\UntBuscaComponentes.pas',
-  UntBuscaComponentesTela in 'D:\Biblioteca\UntBuscaComponentesTela.pas' {FrmBuscaComponentesTela},
-  UntMensagem in 'D:\Biblioteca\UntMensagem.pas' {FrmMensagem},
+  AddOns.View.Principal in 'Src\View\AddOns.View.Principal.pas' {FrmMain},
   Vcl.Themes,
-  Vcl.Styles;
+  Vcl.Styles,
+  Helpers.Functions in '..\Bibliotecas\Helpers\Helpers.Functions.pas',
+  SearchScreen.Controller in '..\Bibliotecas\SearchScreen\SearchScreen.Controller.pas',
+  SearchScreen.View in '..\Bibliotecas\SearchScreen\SearchScreen.View.pas' {FrmBuscaComponentesTela},
+  Msg.Controller in '..\Bibliotecas\Msg\Msg.Controller.pas',
+  Msg.View in '..\Bibliotecas\Msg\Msg.View.pas' {FrmMensagem};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  TStyleManager.TrySetStyle('Silver');
+  TStyleManager.TrySetStyle('Amethyst Kamri');
   Application.CreateForm(TFrmMain, FrmMain);
-  Application.CreateForm(TFrmMensagem, FrmMensagem);
   Application.CreateForm(TFrmBuscaComponentesTela, FrmBuscaComponentesTela);
+  Application.CreateForm(TFrmMensagem, FrmMensagem);
   Application.CreateForm(TFrmBuscaComponentesTela, FrmBuscaComponentesTela);
   Application.CreateForm(TFrmMensagem, FrmMensagem);
   Application.Run;
